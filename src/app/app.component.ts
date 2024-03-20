ccd60512f5f7ca854f7bcaf6e7387856ff348cd3
leowader <101485176+leonardo15LD@users.noreply.github.com>
2024-03-19 21:59:47 -0500

    Add chart and exits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors:{
+      'primary':"#C3083F"
+    }
   },
   plugins: [],
 }
