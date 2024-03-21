8c81ac9ec4de58791311444a60b6cacc83ed188f
KadirQuintero03 <kadireq1928@gmail.com>
2024-03-21 16:12:35 -0500

    Se creo el componente que cargara la grafica dentro del html del home

--- src/app/Home/Home.component.ts
+++ src/app/Home/Home.component.ts
@@ -1,15 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NgApexchartsModule } from 'ng-apexcharts';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { FileService } from '../services/File.service';
-import { ChartOptions, yaxis } from '../share/TypeCharts';
-import { north, south } from '../share/data';
-import { ChartComponent } from 'ng-apexcharts';
 import { FormsModule } from '@angular/forms';
+import { FileService } from '../services/File.service';
 import { variables } from '../interface/variables';
-import { entrenar } from '../share/Entrenamiento';
+import { entrenar } from '../share/training';
+import { GraphicComponent } from '../graphic/graphic.component';
 
 @Component({
   selector: 'app-home',
@@ -17,18 +14,16 @@ import { entrenar } from '../share/Entrenamiento';
   imports: [
     CommonModule,
     RouterModule,
-    NgApexchartsModule,
     HttpClientModule,
     FormsModule,
+    GraphicComponent,
   ],
   templateUrl: './Home.component.html',
 })
 export default class HomeComponent {
-  @ViewChild('chart') chart!: ChartComponent;
-  public chartOptions: Partial<ChartOptions>;
-  variables: variables = new variables();
+  parameters: variables = new variables();
   algoritmo_selec: String = ''; //Almacena el valor del algoritmo que se selecciono
-  archivo: File | null = null;
+  //archivo: File | null = null;
   patrones: number = 0; //Numero de patrones
   entradas: number = 0; //Numero de entradas
   salidas: number = 0; //Numero de salidas
@@ -36,81 +31,23 @@ export default class HomeComponent {
   w: [] = []; //Pesos
   u: [] = []; //Umbrales
 
-  constructor(private fileServices: FileService, private http: HttpClient) {
-    this.chartOptions = {
-      series: [
-        {
-          name: 'north',
-          data: north,
-        },
-        {
-          name: 'south',
-          data: south,
-        },
-      ],
-      chart: {
-        type: 'area',
-        height: 270,
-        width: 550,
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: 'straight',
-      },
-
-      title: {
-        text: 'Area with Negative Values',
-        align: 'left',
-        style: {
-          color: 'white',
-          fontSize: '14px',
-        },
-      },
-      xaxis: {
-        type: 'datetime',
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
-      },
-      yaxis: yaxis,
-      fill: {
-        opacity: 0.5,
-      },
-      tooltip: {
-        x: {
-          format: 'yyyy',
-        },
-        fixed: {
-          enabled: false,
-          position: 'topRight',
-        },
-      },
-      grid: {
-        yaxis: {
-          lines: {
-            offsetX: -30,
-          },
-        },
-        padding: {
-          left: 20,
-        },
-      },
-    };
-  }
+  constructor(private fileServices: FileService, private http: HttpClient) {}
   get(event: any) {
-    this.archivo = event.target.files[0];
-    this.fileServices.mostrar(this.archivo, this.http).subscribe(
+    //Validamos que se haya seleccionado un archivo.
+    if (!event.target.files || event.target.files.length === 0) {
+      alert('No se ha seleccionado ningún archivo');
+      return;
+    }
+
+    //Almacenamos el archivo en una variable.
+    const archivo = event.target.files[0];
+
+    this.fileServices.mostrar(archivo, this.http).subscribe(
       (response) => {
         this.salidas = response[0].salidas;
         this.entradas = response[0].entradas;
         this.patrones = response[0].patrones;
         this.data = response[0].valores;
-        console.log('Response: ', response);
       },
       (error) => {
         console.error('Error al cargar el archivo:', error);
@@ -118,31 +55,38 @@ export default class HomeComponent {
     );
   }
 
+  //Almacenamos el nombre del algoritmo ingresado
   algoritmo_select(nom_algoritmo: String) {
     this.algoritmo_selec = nom_algoritmo;
-    console.log(this.algoritmo_selec);
   }
 
-  Entrenar() {
-    let { num_iteraciones, rata_aprendizaje, error_maximo } = this.variables;
+  //Metodo que llamara a la funcion que ejecutara el algoritmo
+  Entrenamiento() {
+    const { num_iteraciones, rata_aprendizaje, error_maximo } = this.parameters;
 
+    //Validamos que el error maximo permitido no sea menor a 0 ni mayor a 0.1.
     if (num_iteraciones <= 0) {
       return alert('Ingrese un numere de interaciones mayor a 0');
     }
 
+    //Validamos que la rata de aprendizaje no sea menor a 0 ni mayor a 1.
     if (rata_aprendizaje <= 0 || rata_aprendizaje > 1) {
       return alert(
         'La rata de aprendizaje no puede ser menor a 0 ni mayor a 1'
       );
     }
 
+    //Validamos que el error maximo permitido no sea menor a 0 ni mayor a 0.1.
     if (error_maximo < 0 || error_maximo > 0.1) {
       return alert('El error maximo debe ser entre un rango de 0 hasta 0.1');
     }
 
-    console.log('Algoritmo selec: ', this.algoritmo_selec);
-    if (this.algoritmo_selec === 'algoritmo1') {
-      entrenar(this.data, this.variables);
+    //Validamos que el algoritmo que se seleccione no sea diferente de 1.
+    if (this.algoritmo_selec != 'algoritmo1') {
+      return alert('Seleccione un algoritmo de entrenamiento valido');
     }
+
+    //Funcion entrenar de entrenamiento.ts
+    entrenar(this.data, this.parameters);
   }
 }
---
+++ src/app/graphic/graphic.component.ts
@@ -0,0 +1,93 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartComponent } from 'ng-apexcharts';
+import { FormsModule } from '@angular/forms';
+import { ChartOptions, yaxis } from '../share/type_Charts';
+import { north, south } from '../share/data_Charts';
+
+@Component({
+  selector: 'app-graphic',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgApexchartsModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  templateUrl: './graphic.component.html',
+})
+export class GraphicComponent {
+  @ViewChild('chart') chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'north',
+          data: north,
+        },
+        {
+          name: 'south',
+          data: south,
+        },
+      ],
+      chart: {
+        type: 'area',
+        height: 270,
+        width: 550,
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: 'straight',
+      },
+
+      title: {
+        text: 'Area with Negative Values',
+        align: 'left',
+        style: {
+          color: 'white',
+          fontSize: '14px',
+        },
+      },
+      xaxis: {
+        type: 'datetime',
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+      },
+      yaxis: yaxis,
+      fill: {
+        opacity: 0.5,
+      },
+      tooltip: {
+        x: {
+          format: 'yyyy',
+        },
+        fixed: {
+          enabled: false,
+          position: 'topRight',
+        },
+      },
+      grid: {
+        yaxis: {
+          lines: {
+            offsetX: -30,
+          },
+        },
+        padding: {
+          left: 20,
+        },
+      },
+    };
+  }
+}
