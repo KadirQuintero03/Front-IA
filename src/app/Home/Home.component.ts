ced8c191dba578c747e489548fcd7594853006a5
leowader <101485176+leonardo15LD@users.noreply.github.com>
2024-03-20 19:28:12 -0500

    refactoring

--- src/app/Home/Home.component.ts
+++ src/app/Home/Home.component.ts
@@ -4,31 +4,9 @@ import { RouterModule } from '@angular/router';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FileService } from '../services/File.service';
-import {
-  ChartComponent,
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexXAxis,
-  ApexDataLabels,
-  ApexTooltip,
-  ApexStroke,
-  ApexYAxis,
-  ApexTitleSubtitle,
-  ApexFill,
-  ApexGrid,
-} from 'ng-apexcharts';
-export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  xaxis: ApexXAxis;
-  stroke: ApexStroke;
-  tooltip: ApexTooltip;
-  dataLabels: ApexDataLabels;
-  yaxis: ApexYAxis;
-  fill: ApexFill;
-  title: ApexTitleSubtitle;
-  grid: ApexGrid;
-};
+import { ChartOptions,yaxis } from '../share/TypeCharts';
+import { north, south } from '../share/data';
+import { ChartComponent } from 'ng-apexcharts';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -43,179 +21,17 @@ export default class HomeComponent {
   entradas = 0;
   salidas = 0;
   patrones = 0;
-  data = [];
+
   constructor(private prueba: FileService, private http: HttpClient) {
     this.chartOptions = {
       series: [
         {
           name: 'north',
-          data: [
-            {
-              x: 1996,
-              y: 322,
-            },
-            {
-              x: 1997,
-              y: 324,
-            },
-            {
-              x: 1998,
-              y: 329,
-            },
-            {
-              x: 1999,
-              y: 342,
-            },
-            {
-              x: 2000,
-              y: 348,
-            },
-            {
-              x: 2001,
-              y: 334,
-            },
-            {
-              x: 2002,
-              y: 325,
-            },
-            {
-              x: 2003,
-              y: 316,
-            },
-            {
-              x: 2004,
-              y: 318,
-            },
-            {
-              x: 2005,
-              y: 330,
-            },
-            {
-              x: 2006,
-              y: 355,
-            },
-            {
-              x: 2007,
-              y: 366,
-            },
-            {
-              x: 2008,
-              y: 337,
-            },
-            {
-              x: 2009,
-              y: 352,
-            },
-            {
-              x: 2010,
-              y: 377,
-            },
-            {
-              x: 2011,
-              y: 383,
-            },
-            {
-              x: 2012,
-              y: 344,
-            },
-            {
-              x: 2013,
-              y: 366,
-            },
-            {
-              x: 2014,
-              y: 389,
-            },
-            {
-              x: 2015,
-              y: 334,
-            },
-          ],
+          data: north,
         },
         {
           name: 'south',
-          data: [
-            {
-              x: 1996,
-              y: 162,
-            },
-            {
-              x: 1997,
-              y: 90,
-            },
-            {
-              x: 1998,
-              y: 50,
-            },
-            {
-              x: 1999,
-              y: 77,
-            },
-            {
-              x: 2000,
-              y: 35,
-            },
-            {
-              x: 2001,
-              y: -45,
-            },
-            {
-              x: 2002,
-              y: -88,
-            },
-            {
-              x: 2003,
-              y: -120,
-            },
-            {
-              x: 2004,
-              y: -156,
-            },
-            {
-              x: 2005,
-              y: -123,
-            },
-            {
-              x: 2006,
-              y: -88,
-            },
-            {
-              x: 2007,
-              y: -66,
-            },
-            {
-              x: 2008,
-              y: -45,
-            },
-            {
-              x: 2009,
-              y: -29,
-            },
-            {
-              x: 2010,
-              y: -45,
-            },
-            {
-              x: 2011,
-              y: -88,
-            },
-            {
-              x: 2012,
-              y: -132,
-            },
-            {
-              x: 2013,
-              y: -146,
-            },
-            {
-              x: 2014,
-              y: -169,
-            },
-            {
-              x: 2015,
-              y: -184,
-            },
-          ],
+          data: south,
         },
       ],
       chart: {
@@ -247,24 +63,7 @@ export default class HomeComponent {
           show: false,
         },
       },
-      yaxis: {
-        tickAmount: 4,
-        floating: false,
-
-        labels: {
-          style: {
-            colors: '#8e8da4',
-          },
-          offsetY: -7,
-          offsetX: 0,
-        },
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
-      },
+      yaxis: yaxis,
       fill: {
         opacity: 0.5,
       },
@@ -291,18 +90,14 @@ export default class HomeComponent {
   }
   get(event: any) {
     this.archivo = event.target.files[0];
-
     this.prueba.mostrar(this.archivo, this.http).subscribe(
       (response) => {
-        // Manejar la respuesta del servidor
-
         console.log('Respuesta del servidor:', response[0].salidas);
         this.salidas = response[0].salidas;
         this.entradas = response[0].entradas;
         this.patrones = response[0].patrones;
       },
       (error) => {
-        // Manejar errores en la solicitud
         console.error('Error al cargar el archivo:', error);
       }
     );
