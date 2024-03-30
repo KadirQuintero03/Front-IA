f7e94dd29b34a4e770a714661cb165101238cb79
KadirQuintero03 <kadireq1928@gmail.com>
2024-03-30 16:18:58 -0500

    arreglao

--- src/app/utils/training.ts
+++ src/app/utils/training.ts
@@ -8,15 +8,6 @@ export function entrenar(
   n: number,
   numPatrones: number
 ) {
-  //Salida de la funcion soma
-  let sumaEpatron = 0;
-  var SI = []; //Salidas y1 y2
-  var si: any; //Se aplica la funcion de activacion 0 o 1
-  var error_lineal: any; //Error lineal
-  let error_linealV = []; //Guardamos los errores lineales en un vector
-  let suma_error_lineal = 0; //Suma error lineal
-  let error_patron = []; //Error patron
-  WyU.u = [0.5, 0.3]; //Umbral
   WyU.w = [
     [1, 1],
     [0.5, 0.5],
@@ -29,18 +20,21 @@ export function entrenar(
   let cont = 1;
   console.log('entradas', data[0].entradasValue[1]);
   console.log('salidas', data[1].salidasValue[1]);
-
+  // salida de la red en el primer patron S1=(s1*w11+s2*w21+s3*w31)
+  // salida de la red en el primer patron S2=(s1*w12+s2*w22+s3*w32)
   let suma = 0;
   var SI = []; //salidas y1 y2
   var si: any;
   var el: any;
   let vectorAux: number[] = [];
   console.log('error maximo: ', Parameters.error_maximo);
+  // 1            0.05
   let i = 0;
   for (let f = 0; f < numPatrones; f++) {
     for (let c = 0; c < m; c++) {
       vectorAux[i] = data[0].entradasValue[f][c];
       i++;
+      //console.log(data[0].entradasValue[f][c])
     }
     for (let k = 0; k < m; k++) {
       for (let j = 0; j < n; j++) {
@@ -54,39 +48,6 @@ export function entrenar(
     i = 0;
     suma = 0;
   }
-}
-/*const Ss = (data: any, m: number, n: number, patrones: number, pesos: any) => {
-  let suma;
-  for (let k = 0; k < patrones; k++) {
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < m; j++) {
-        let pesoN =
-          WyU.w[j][i] +
-          Parameters.rata_aprendizaje * eli[i] * data[1].salidasValue[j][i]; //CALCULANDO PESOS
-        // WyU.w = [
-        //   [0.5, 0.8],
-        //   [0.3, -1],
-        //   [0.4, 0],
-        // ];
-
-        WyU.w[j][i] = pesoN; //MODIFICANDO PESOS
-      }
-    }
-    for (let i = 0; i < n; i++) {
-      let Unew = WyU.u[i] + Parameters.rata_aprendizaje * eli[i] * 1;
-
-      WyU.u[i] = Unew;
-    }
-    console.log('PESO NEW', WyU.w);
-    console.log('UMBRAL NEW', WyU.u);
-    let sumaEpatron = 0;
-    for (let i = 0; i < epatron.length; i++) {
-      sumaEpatron += epatron[i];
-    }
-    eit = sumaEpatron / numPatrones;
-    console.log('ETI', eit);
-  }
-
   // 0.5 > 0.05
   // while (eit > Parameters.error_maximo) {
   //   console.log('eit', cont, eit);
@@ -169,4 +130,17 @@ export function entrenar(
   //   }
 
   // }
-}*/
+}
+const Ss = (data: any, m: number, n: number, patrones: number, pesos: any) => {
+  let suma;
+  for (let k = 0; k < patrones; k++) {
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        suma = data[0].entradasValue[k][j] * pesos.w[j][i];
+        console.log('suma', suma);
+      }
+      console.log('pase de columna en pesos');
+    }
+    console.log('pase de patron');
+  }
+};
